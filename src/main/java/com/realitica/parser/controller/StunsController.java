eabed2f802632854a843fcebba73bb95efc66af6
roman.struchev <roman.struchev@sibit.tech>
2021-04-03 09:46:40 +0200

    add type column and ordering by date

--- src/main/java/com/realitica/parser/controller/StunsController.java
+++ src/main/java/com/realitica/parser/controller/StunsController.java
@@ -3,10 +3,12 @@ package com.realitica.parser.controller;
 import com.realitica.parser.entity.Stun;
 import com.realitica.parser.repo.StunRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -18,7 +20,7 @@ public class StunsController {
 
     @GetMapping(path = "stuns")
     public ModelAndView stuns() {
-        List<Stun> stunsList = stunRepository.findAll();
+        List<Stun> stunsList = stunRepository.findAll(Sort.by(Sort.Direction.DESC, "lastModified"));
         Map attributes = new HashMap<>();
         attributes.put("stuns", stunsList);
         return new ModelAndView("stuns", attributes);
