bbb3419b2f6fae8e39d9b2fab74eafe950f4ad87
Struchev Roman Konstantinovich <struchev-rk@fxclub.org>
2021-12-03 16:27:03 +0100

    deploy to ztv

--- src/main/java/com/realitica/parser/controller/StunsController.java
+++ src/main/java/com/realitica/parser/controller/StunsController.java
@@ -2,24 +2,36 @@ package com.realitica.parser.controller;
 
 import com.realitica.parser.entity.Stun;
 import com.realitica.parser.repo.StunRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@Slf4j
+@RequiredArgsConstructor
 @Controller
 public class StunsController {
-    @Autowired
-    StunRepository stunRepository;
+    private final StunRepository stunRepository;
 
     @GetMapping(path = "stuns")
-    public ModelAndView stuns() {
+    public ModelAndView stuns(@RequestHeader String host,
+                              @RequestParam(name = "redirect", defaultValue = "false") Boolean redirect) {
+        // 302 redirect from deprecated domaim
+        if (redirect != null && redirect && host.contains("heroku")) {
+            log.info("Request from {}, redirect to realitica.struchev.site", host);
+            return new ModelAndView("redirect:/realitica.struchev.site");
+        } else {
+            log.info("Request from {}, use this host", host);
+        }
+
         List<Stun> stunsList = stunRepository.findAll(Sort.by(Sort.Direction.DESC, "lastModified"));
         Map attributes = new HashMap<>();
         attributes.put("stuns", stunsList);
