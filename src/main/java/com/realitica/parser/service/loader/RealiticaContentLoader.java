e36122ca88616f485367035374d5c1a921e5118f
Roman Struchev <underpender@yandex.ru>
2025-03-11 18:01:43 +0100

    Add source to table

--- src/main/java/com/realitica/parser/service/loader/RealiticaContentLoader.java
+++ src/main/java/com/realitica/parser/service/loader/RealiticaContentLoader.java
@@ -5,12 +5,12 @@ import com.realitica.parser.repository.AdRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Element;
 import org.jsoup.parser.Tag;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
 
 import java.io.IOException;
 import java.time.LocalDate;
